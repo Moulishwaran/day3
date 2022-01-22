738cdc81aede5f5f28e8d9bc26f40da670c1a60f
Moulishwaran <88322031+Moulishwaran@users.noreply.github.com>
2022-01-22 14:03:15 +0530

    Add files via upload

---
+++ day3 ass1.js
@@ -0,0 +1,29 @@
+
+    "basics"; {
+      "name"; "Moulishwaran",
+      "email";"moulish0809@gmail.com",
+      "phone";"+917502044117",
+      "degree"; "BE",
+      "location"; {
+        "address"; "thanjavur",
+        "postalCode"; "613602"
+        "city"; "Thanjavur"
+        "State; Tamilandau "
+        "country; India"
+      }
+      "work";[
+        {
+          "company": "Sanmina india pvt ltd",
+          "position": "trainee",
+          "startDate": "2019-02-05",
+          "endDate":"2021-10-21",
+          "summary":"i am a trainee in tha compaany",
+          
+        }
+      ]
+      "languages"; [
+        {
+          "language": "tamil,english",
+          "fluency": "english,tamil"
+        }
+      }
